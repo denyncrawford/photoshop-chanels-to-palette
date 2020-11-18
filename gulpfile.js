d409fd083f7283425d4f7551cd63d308a1643d66
denyncrawford <kiquerangelb@gmail.com>
2020-11-18 14:26:19 -0300

    initial comit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var run = require('gulp-run');
+const { watch } = require('gulp');
+
+const js = (cb) => {
+    run('npm run build').exec()
+     cb()
+ }
+
+function defaultTask(cb) {
+    watch('src/*.js', js);
+    cb();
+  }
+  
+exports.default = defaultTask
---
+++ src/palettepicker.js
@@ -0,0 +1,16 @@
+const doc = app.activeDocument;
+const layer = doc.activeLayer;
+const myActiveChannels = doc.activeChannels || []
+
+let channels = []
+
+myActiveChannels.forEach(channel => {
+    let name = channel.name
+    let color = {}
+    if (channel.kind == "ChannelType.SPOTCOLOR")  color = channel.color.rgb;
+    channels.push({name, color})
+})
+
+app.documents.add(21, 29, 300, "paleta", NewDocumentMode.RGB)
+
+alert("My Active Channels Info\n" + channels );
