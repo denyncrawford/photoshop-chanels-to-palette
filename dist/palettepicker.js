0c99e5268577f53b57159d016df3b34e2b8f9c87
denyncrawford <kiquerangelb@gmail.com>
2020-11-18 15:22:28 -0300

    socument generation succeed

--- dist/palettepicker.js
+++ dist/palettepicker.js
@@ -892,6 +892,23 @@ myActiveChannels.forEach(function (channel) {
   });
 });
 app.documents.add(21, 29, 300, "paleta", NewDocumentMode.RGB);
+
+var textLayer = function textLayer(text) {
+  var layers = app.activeDocument.artLayers;
+  var layer = layers.add();
+  layer.kind = LayerKind.TEXT;
+  var textItem = layer.textItem;
+  textItem.kind = TextType.PARAGRAPHTEXT;
+  textItem.size = 30;
+  textItem.position = [10, 10];
+  textItem.contents = text;
+  textItem.width = new UnitValue("300 centimeter");
+  textItem.height = new UnitValue("30 pixels");
+};
+
+channels.forEach(function (channel) {
+  textLayer(channel.name);
+});
 alert("My Active Channels Info\n" + channels);
 
 },{}]},{},[1]);
--- src/palettepicker.js
+++ src/palettepicker.js
@@ -13,4 +13,21 @@ myActiveChannels.forEach(channel => {
 
 app.documents.add(21, 29, 300, "paleta", NewDocumentMode.RGB)
 
+const textLayer = (text) => {
+    var layers = app.activeDocument.artLayers;
+    var layer = layers.add();
+    layer.kind = LayerKind.TEXT;
+    var textItem = layer.textItem;
+    textItem.kind = TextType.PARAGRAPHTEXT;
+    textItem.size = 30;
+    textItem.position = [10, 10];
+    textItem.contents = text;
+    textItem.width = new UnitValue("300 centimeter");
+    textItem.height = new UnitValue("30 pixels");
+}
+
+channels.forEach(channel => {
+    textLayer(channel.name) 
+})
+
 alert("My Active Channels Info\n" + channels );
